85fab1fa2b10b2f4cd805ffff772f0935ff01bc6
Rkane <rkane@classlink.com>
2018-11-21 11:55:39 -0500

    Preparation for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var application = express();
 
@@ -61,6 +62,6 @@ application.get('/bad', (req, res) => {
     });
 });
 
-application.listen(3000, () => {
-    console.log('Server is running on port 3000');
+application.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
